78e7c5c1c2b4438f1370d6ed0557e9c9c13aece7
Steve Landey <steve@steveasleep.com>
2024-08-16 16:36:21 -0700

    Configure djot to give us source positions and warnings

--- src/djotLogic.ts
+++ src/djotLogic.ts
@@ -4,5 +4,8 @@ import { parse, type Doc } from "@djot/djot";
 export function parseDjot(path: string): Doc {
   console.log(parse);
   console.log(path);
-  return parse(fs.readFileSync(path, "utf8"));
+  return parse(fs.readFileSync(path, "utf8"), {
+    sourcePositions: true,
+    warn: (warning) => console.warn(warning.render()),
+  });
 }
