611b3af79685559540e778d768b70ee05004ee2e
Steve Landey <steve@steveasleep.com>
2024-09-05 08:41:08 -0700

    Link resolution is more predictable and less overall stupid (#47)
    * Link resolution is more predictable and less overall stupid
    
    * Update typedoc link mapping
    
    * Go back to resolving hashed mapped links correctly
    
    * Fixes for mapped links

--- src/plugins/linkRewritingPlugin.test.ts
+++ src/plugins/linkRewritingPlugin.test.ts
@@ -1,7 +1,47 @@
-import { resolveRelativePath } from "./linkRewritingPlugin.js";
+import { Doc } from "@djot/djot";
+import {
+  MappableLinkTarget,
+  resolveRelativeRefPath,
+} from "./linkRewritingPlugin.js";
 
 test("Resolves explicit relative links", () => {
-  expect(resolveRelativePath("a/b/c.txt", "./d.txt")).toEqual("/a/b/d.txt");
-  expect(resolveRelativePath("a/b/c.txt", "./../d.txt")).toEqual("/a/d.txt");
-  expect(resolveRelativePath("a/b/c.txt", "../d.txt")).toEqual("/a/d.txt");
+  expect(resolveRelativeRefPath("a/b/c.txt", "./d.txt")).toEqual("/a/b/d.txt");
+  expect(resolveRelativeRefPath("a/b/c.txt", "./../d.txt")).toEqual("/a/d.txt");
+  expect(resolveRelativeRefPath("a/b/c.txt", "../d.txt")).toEqual("/a/d.txt");
+});
+
+test("MappableLinkTarget has expected values", () => {
+  const stubDoc: Doc = {
+    tag: "doc",
+    references: {},
+    autoReferences: {},
+    footnotes: {},
+    children: [],
+  };
+  const t = new MappableLinkTarget(
+    {
+      docs: {
+        content: stubDoc,
+      },
+      title: "The Doc",
+      titleAST: [],
+      originalExtension: ".djot",
+      fsPath: "/fsroot/input/subdir/the_doc.djot",
+      refPath: "subdir/the_doc",
+      filename: "the_doc",
+      frontMatter: {},
+      data: {},
+    },
+    "the-hash"
+  );
+
+  expect(t.aliases).toEqual([
+    "#the-hash",
+    "the_doc#the-hash",
+    "the_doc.djot#the-hash",
+    "subdir/the_doc#the-hash",
+    "subdir/the_doc.djot#the-hash",
+    "/subdir/the_doc#the-hash",
+    "/subdir/the_doc.djot#the-hash",
+  ]);
 });
