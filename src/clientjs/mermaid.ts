c09a99ecdc954ef557f026cbc73593cc3171f464
Steve Landey <steve@steveasleep.com>
2024-09-08 21:47:48 -0700

    Mermaid dark mode

--- src/clientjs/mermaid.ts
+++ src/clientjs/mermaid.ts
@@ -1,11 +1,20 @@
 import mermaid from "mermaid";
 
 window.addEventListener("dj-onload", () => {
+  const IS_DARK_MODE =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
   // Replace all <pre><code class="language-mermaid">...</code></pre> with just <pre class="mermaid"></pre>
   [...document.querySelectorAll("pre code.language-mermaid")].map((codeEl) => {
     const parent = codeEl.parentElement!;
     parent.innerHTML = codeEl.innerHTML;
     parent.className = "language-mermaid";
+  });
+
+  mermaid.initialize({
+    theme: IS_DARK_MODE ? "dark" : "default",
+    darkMode: IS_DARK_MODE,
   });
 
   mermaid.run({ querySelector: ".language-mermaid" });
